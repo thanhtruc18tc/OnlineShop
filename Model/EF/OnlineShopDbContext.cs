6e87a6e116f9f1907a94072ccc947473b8bcafc8
Truc Tran <thanhtruc18tc@gmail.com>
2019-09-21 18:11:18 +0700

    first commit

---
+++ Model/EF/OnlineShopDbContext.cs
@@ -0,0 +1,36 @@
+namespace Model.EF
+{
+    using System;
+    using System.Data.Entity;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Linq;
+
+    public partial class OnlineShopDbContext : DbContext
+    {
+        public OnlineShopDbContext()
+            : base("name=OnlineShopDbContext")
+        {
+        }
+
+        public virtual DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>()
+                .Property(e => e.UserName)
+                .IsFixedLength();
+
+            modelBuilder.Entity<User>()
+                .Property(e => e.Password)
+                .IsFixedLength();
+
+            modelBuilder.Entity<User>()
+                .Property(e => e.Name)
+                .IsFixedLength();
+
+            modelBuilder.Entity<User>()
+                .Property(e => e.Address)
+                .IsFixedLength();
+        }
+    }
+}
